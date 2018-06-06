0f18e4f59db7916cf96e3d2feb28c46e15bd9079
Taylor-Zapalac <Taylor.Zapalac@gmail.com>
2018-06-06 11:05:59 -0500

    Implementation problems

---
+++ DailyCode/Implementation Challenges/AppleAndOrange.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyCode.Implementation_Challenges
+{
+    class AppleAndOrange
+    {
+        // The problem on hackerrank is overexplained
+        // Given a range on a line, a is apple tree location, b is orange tree location
+        // s is start of area, t is end of area.
+        // given m, number of apples fell, apples - location of apples
+        // given n number of oranges fell, oranges - location of oranges
+        // how many fell between s and t
+
+        public void count(int startArea, int endArea, int appleTreeLocation, int orangeTreeLocation, int[] apples, int[] oranges)
+        {
+            int appleHitCount = 0;
+            int orangeHitCount = 0;
+
+            foreach(int i in apples)
+            {
+                int dropPoint = i + appleTreeLocation;
+                if(dropPoint >= startArea && dropPoint <= endArea)
+                {
+                    appleHitCount++;
+                }
+            }
+
+            foreach (int j in oranges)
+            {
+                int dropPoint = j + orangeTreeLocation;
+                if (dropPoint >= startArea && dropPoint <= endArea)
+                {
+                    orangeHitCount++;
+                }
+            }
+
+            Console.WriteLine(appleHitCount);
+            Console.WriteLine(orangeHitCount);
+
+        }
+    }
+}
---
+++ DailyCode/Implementation Challenges/GradingStudents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyCode.Implementation_Challenges
+{
+    class GradingStudents
+    {
+        //Every student receives a grade in the inclusive range from 0 to 100.
+        //Any grade less than 40 is a failing grade.
+        // If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+        // If the value of  is less than 38, no rounding occurs as the result will still be a failing grade.
+
+        //Input n = number of students, int[] grade, each students grade
+
+        public int[] roundGrade(int students, int[] grades)
+        {
+
+
+            return null;
+        }
+    }
+}
